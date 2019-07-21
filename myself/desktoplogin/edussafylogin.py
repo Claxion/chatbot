f7af374693782cd6bad2db5c4d3a6365e6711c1c
EuiJin <kup90@naver.com>
2019-07-22 01:28:50 +0900

    loginmore

--- myself/desktoplogin/edussafylogin.py
+++ myself/desktoplogin/edussafylogin.py
@@ -1,12 +1,17 @@
 import requests
 import bs4
 from selenium import webdriver
+import time
+import chromedriver_binary
+
+#https://pypi.org/project/chromedriver-binary/75.0.3770.140.0/
 
 def login(inputusername, inputuserpwd, islogin):
     # inputusername 사용자 이름 inputuserpwd 사용자 비밀번호 islogin 입실?퇴실?
     
-    chromedriver_dir = r'C:\Users\student\Downloads\chromedriver_win32\chromedriver.exe'
-    driver = webdriver.Chrome(chromedriver_dir)
+    #chromedriver_dir = r'd:\SSAFY2019\chatbot\myself\desktoplogin\chromedriver.exe'
+    #driver = webdriver.Chrome(chromedriver_dir)
+    driver = webdriver.Chrome()
 
     loginurl = "https://edu.ssafy.com/comm/login/SecurityLoginForm.do"
     driver.get(loginurl)
@@ -16,7 +21,7 @@ def login(inputusername, inputuserpwd, islogin):
     password = driver.find_element_by_id("userPwd")
     password.clear()
     password.send_keys(inputuserpwd)
-
+    time.sleep(1)
     #driver.find_element_by_name("로그인").click()
     #driver.find_element_by_link_text("로그인").click()
     #driver.find_element_by_class_name("form-btn").click()
@@ -26,16 +31,18 @@ def login(inputusername, inputuserpwd, islogin):
     sessionurl = "http://edu.ssafy.com/edu/main/index.do"
     driver.get(sessionurl)
 
-    if True:
-        xpath = '//*[@id="checkIn"]/span' #입실
-    else:
-        xpath = '//*[@id="checkOut"]/span'   #퇴실
+    # if True:
+    #     xpath = '//*[@id="checkIn"]/span' #입실
+    # else:
+    #     xpath = '//*[@id="checkOut"]/span'   #퇴실
     xpath = '//*[@id="_mainComunityId"]/div[2]/div[1]/article/a/span' # 알림
 
     btn = driver.find_element_by_xpath(xpath)
     # 자바 스크립트가 있을 경우에는 실행 코드를 넣어줘야함.
     # http://blog.naver.com/PostView.nhn?blogId=kiddwannabe&logNo=221430636045&categoryNo=0&parentCategoryNo=0&viewDate=&currentPage=1&postListTopCurrentPage=1&from=postView
     driver.execute_script("arguments[0].click();", btn)
+    time.sleep(2)
+    #driver.quit()
 
 
 
--- myself/desktoplogin/login.py
+++ myself/desktoplogin/login.py
@@ -10,9 +10,9 @@ from tkinter import *
 from selenium import webdriver
 import os
 from datetime import datetime
-
-
+import chromedriver_binary
 import edussafylogin
+
 
 def login():
     username_info = username.get()
@@ -22,40 +22,36 @@ def login():
     loginfile.close()
     edussafylogin.login(username_info, password_info, True)
     
-    
-    
 def logout():
     username_info = username.get()
     password_info = password.get()
     edussafylogin.login(username_info, password_info, False)
+    
+def reset_screen():
+    yesBtn.forget()
+    noBtn.forget()
+    loginBtn.pack()
+    logoutBtn.pack()
+    sixLabel.forget()
 
-# print(os.path.expanduser('~'))
-# def register_user():
 
-#     username_info = username.get()
-#     password_info = password.get()
-
-#     file = open(username_info, "w")
-#     file.write(username_info + "\n")
-#     file.write(password_info)
-#     file.close()
-
-#     username_entry.delete(0, END)
-#     password_entry.delete(0, END)
-
-#     Label(register_screen, text="Registration Success", fg="green", font=("calibri", 11)).pack()
+def logoutcheck():
+    if datetime.now().hour <= 24:
+        global sixLabel
+        sixLabel = Label(main_screen, text = '6시 전입니다.\n 진짜 퇴실하시겠습니까?')
+        sixLabel.pack()
+        loginBtn.forget()
+        logoutBtn.forget()
+        global yesBtn
+        global noBtn
+        yesBtn = Button(text="Yes", height="1", width="15", command = logout)
+        noBtn = Button(text="No", height="1", width="15", command=reset_screen)
+        yesBtn.pack()
+        noBtn.pack()
+    else:
+        logout()      
 
 # Designing Main(first) window
-def checktime_screen():
-    global checktime_screen
-    checktime_screen = Toplevel(main_screen)
-    checktime_screen.title('퇴실시간체크')
-    checktime_screen.geometry('200x100')
-    Label(checktime_screen, text = '6시 전입니다.\n 진짜 퇴실하시겠습니까?').pack()
-    Button(checktime_screen, text='yes',width=10, height = 1, command='').pack()
-    Button(checktime_screen, text='no',width=10, height = 1, command='').pack()
-    checktime_screen.lift()
-
 def main_account_screen():
     global main_screen
     main_screen = Tk()
@@ -77,27 +73,20 @@ def main_account_screen():
     Label(text="Username * ").pack()
     username_login_entry = Entry(main_screen, textvariable=username)
     username_login_entry.pack()
-    Label(text="").pack()
     Label(text="Password * ").pack()
     password_login_entry = Entry(main_screen, textvariable=password, show= '*')
     password_login_entry.pack()
     Label(text="").pack()
-    Button(text="입실", height="2", width="30", command = login).pack()
-    Label(text="").pack()
-    Button(text="퇴실", height="2", width="30", command= logout).pack()
-    
-    if datetime.now().hour <= 24:
-        checktime_screen()
-    else :
-        pass
-    main_screen.iconify()
+    global loginBtn
+    global logoutBtn
+    loginBtn = Button(text="입실", height="1", width="15", command = login)
+    logoutBtn = Button(text="퇴실", height="1", width="15", command= logoutcheck)
+    loginBtn.pack()
+    logoutBtn.pack()
+
     main_screen.mainloop()
 
 
-
+#start of program
 homepath = os.path.expanduser('~')
-
-
 main_account_screen()
-
-#%%
